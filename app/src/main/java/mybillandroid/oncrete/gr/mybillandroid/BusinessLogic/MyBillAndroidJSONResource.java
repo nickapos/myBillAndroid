71ab5d8d20a63f335a2deb6baa4d1c50161499cb
Nick Apostolakis <nickapos@oncrete.gr>
2015-02-15 10:49:38 +0000

    modifying the project layout. adding a gson import

--- app/src/main/java/mybillandroid/oncrete/gr/mybillandroid/BusinessLogic/MyBillAndroidJSONResource.java
+++ app/src/main/java/mybillandroid/oncrete/gr/mybillandroid/BusinessLogic/MyBillAndroidJSONResource.java
@@ -1,4 +1,6 @@
-package mybillandroid.oncrete.gr.mybillandroid;
+package mybillandroid.oncrete.gr.mybillandroid.BusinessLogic;
+
+import com.google.gson.Gson;
 
 /**
  * Created by nickapos on 15/02/15.
@@ -6,4 +8,5 @@ package mybillandroid.oncrete.gr.mybillandroid;
 public class MyBillAndroidJSONResource {
     String basicURL = "http://localhost:8000";
     String apiEndPoint = "/api/v1/";
+    Gson gson = new Gson();
 }
