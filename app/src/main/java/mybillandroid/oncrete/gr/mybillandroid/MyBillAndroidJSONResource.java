1ed6017901f1a887b3f1da267c773c36ed23ebda
Nick Apostolakis <nickapos@oncrete.gr>
2015-02-15 08:12:34 +0000

    adding gson dependency and stub json file

---
+++ app/src/main/java/mybillandroid/oncrete/gr/mybillandroid/MyBillAndroidJSONResource.java
@@ -0,0 +1,9 @@
+package mybillandroid.oncrete.gr.mybillandroid;
+
+/**
+ * Created by nickapos on 15/02/15.
+ */
+public class MyBillAndroidJSONResource {
+    String basicURL = "http://localhost:8000";
+    String apiEndPoint = "/api/v1/";
+}
