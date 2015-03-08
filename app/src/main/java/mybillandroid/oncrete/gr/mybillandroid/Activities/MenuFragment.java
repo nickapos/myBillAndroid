5236bf1e206381c5a2434ef6826f513af0b9007e
Nick Apostolakis <nickapos@oncrete.gr>
2015-03-08 11:09:30 +0000

    adding a layout for internal fragment items

--- app/src/main/java/mybillandroid/oncrete/gr/mybillandroid/Activities/MenuFragment.java
+++ app/src/main/java/mybillandroid/oncrete/gr/mybillandroid/Activities/MenuFragment.java
@@ -12,12 +12,12 @@ import android.widget.ListView;
  * Created by nickapos on 08/03/15.
  */
 public class MenuFragment extends ListFragment {
-    String[] AndroidOS = new String[]{"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream SandWich", "Jelly Bean", "KitKat"};
-    String[] Version = new String[]{"1.5", "1.6", "2.0-2.1", "2.2", "2.3", "3.0-3.2", "4.0", "4.1-4.3", "4.4"};
+    String[] AndroidOS = new String[]{"Expenses", "Income", "Companies", "Categories", "Reports"};
+    String[] Version = new String[]{"1.5", "1.6", "2.0-2.1", "2.2", "2.3"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        View view = inflater.inflate(R.layout.main_list_fragment, container, false);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, AndroidOS);
         setListAdapter(adapter);
